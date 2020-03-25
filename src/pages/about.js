3e4bfb22cd9781413c68b09183db5e83fb6e1538
Andrew Sun <ajsun12@gmail.com>
2020-03-24 22:27:34 -0400

    adding about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,12 +8,20 @@ class AboutIndex extends React.Component {
     render() {
         const siteTitle = get(this, 'props.data.site.siteMetadata.title')
         return (
-            <Layout location={this.props.location}>
-                <Helmet title={siteTitle} />
-                <div>
-                    Thoughtful and interesting questions updated Monday, Wednesday and Friday
-                </div>
-            </Layout>
+          <Layout location={this.props.location}>
+            <Helmet title={siteTitle} />
+            <div>
+              <p>
+                Thoughtful and interesting questions updated Monday, Wednesday and Friday
+              </p>
+              <h4>How it works</h4>
+              <ul>
+              <li>Every Monday, Wednesday, and Friday morning, we'll send your group a unique question</li>
+              <li>Submit your answers on the website before 3pm ET</li>
+              <li>Shortly after the question closes, we'll send the answers back for you to enjoy!</li>
+              </ul>
+            </div>
+          </Layout>
         )
     }
 }
